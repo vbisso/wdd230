00e88538108349efdeddc0a02e80b9f51d2b16f4
vbisso <valeriabisso19@gmail.com>
2024-01-30 09:07:44 -0700

    Home Page Enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -6,3 +6,21 @@ document.getElementById("copyright").innerHTML =`&copy;${currentYear} | Valeria 
 //get last modified
 let lastModifiedDate = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modified: ${lastModifiedDate}`;
+
+
+//responsive menu nav
+const buttonElement = document.querySelector('#button');
+const menuListElement = document.querySelector('.menuList');
+
+buttonElement.addEventListener('click', () => {
+    menuListElement.classList.toggle('open');
+    buttonElement.classList.toggle('open');
+})
+
+//DARK MODE
+const darModeElement = document.querySelector('#darkMode');
+
+
+darModeElement.addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('dark');
+})
