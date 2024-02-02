dd24267cba11e6c0a8d8eda1f6a1a2b853bdb300
vbisso <valeriabisso19@gmail.com>
2024-02-02 14:04:16 -0700

    chamber first commit

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,52 @@
+const menuButn = document.querySelector('#menuButton');
+const navList = document.querySelector('.navigationList');
+const navigation = document.querySelector('nav');
+const body = document.body;
+
+
+menuButn.addEventListener('click', () => {
+    navList.classList.add('open'); 
+    menuButn.classList.add('open');
+    
+    const newPage = document.createElement('div');
+    newPage.classList.toggle('new-page');
+
+    newPage.appendChild(navList); //Appends the navigation list to the new page (div new-page) and not the navList);//Appends the navigation list to the new page
+    body.appendChild(newPage); //Appends the new page to the body    
+
+    const newButton = document.createElement('button');
+    newButton.textContent = 'X';
+    newButton.classList.toggle('new-button');
+    newPage.appendChild(newButton);
+
+    newButton.addEventListener('click', () => {
+        navList.classList.remove('open');
+        menuButn.classList.remove('open');
+        navigation.appendChild(navList);
+        body.removeChild(newPage);
+    })
+
+    window.addEventListener('resize', function() {
+        const screenWidth = window.innerWidth || document.documentElement.clientWidth;
+
+        if (screenWidth >= 650) {
+            navList.classList.remove('open');
+            menuButn.classList.remove('open');
+            navigation.appendChild(navList);
+            if (body.contains(newPage)) {
+                // Remove newPage from the body
+                body.removeChild(newPage);
+            }
+        }
+    });
+
+
+
+
+   
+})
+
+
+
+
+    
