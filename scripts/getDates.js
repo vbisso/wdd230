cd516ceb31f88d1a59dc835081165320b9a4a4e6
vbisso <valeriabisso19@gmail.com>
2024-02-09 13:48:06 -0700

    w03 team activity

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -24,3 +24,17 @@ const darModeElement = document.querySelector('#darkMode');
 darModeElement.addEventListener('click', () => {
     document.querySelector('body').classList.toggle('dark');
 })
+
+//PAGE VISITIS!!    
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsDisplay.textContent = numVisits;
+}
+    else{
+        visitsDisplay.innerHTML = " This is your first visit 🥹 <br> Welcome!! ";
+    }
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
