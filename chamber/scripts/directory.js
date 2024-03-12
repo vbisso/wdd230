f884432d73df32ad9c96b371f2385520e53a08a5
vbisso <valeriabisso19@gmail.com>
2024-03-12 09:48:23 -0600

    directory updates

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,53 @@
 //MEMBERS JSON
-const url = 'https://vbisso.github.io/wdd230/chamber/data/members.json';
+const url = 'https://raw.githubusercontent.com/vbisso/wdd230/master/chamber/data/members.json';
 const article = document.querySelector('article');
+async function getMembers(){
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data);
+}
+function displayMembers(data){
+
+    for (let i = 0; i < data.companies.length; i++){
+        const member = data.companies[i];
+        const card = document.createElement('section');
+        card.setAttribute('class', 'card');
+        const name = document.createElement('h3');
+        const address = document.createElement('p');
+        const phone = document.createElement('p');
+        const website = document.createElement('p');
+        const image = document.createElement('img');
+
+        name.textContent = member.name;
+        name.setAttribute('class', 'name');
+        address.textContent = member.address;
+        phone.textContent = member.phone;
+        website.textContent = member.website;
+        website.setAttribute('class', 'website');
+        image.setAttribute('src', member.image);
+        image.setAttribute('alt', member.name);
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        article.appendChild(card);
+
+}}
+
+
+getMembers();
+
+//GRID AND LIST VIEWS
+const gridBtn = document.querySelector('#gridBtn');
+const listBtn = document.querySelector('#listBtn');
+
+gridBtn.addEventListener('click', ()=>{
+    article.classList.remove('list');
+    article.classList.add('grid');
+})
+listBtn.addEventListener('click', ()=>{
+    article.classList.remove('grid');
+    article.classList.add('list');
+
+})
--- scripts/weatherAPI.js
+++ scripts/weatherAPI.js
@@ -1,8 +1,8 @@
 
-//43.82347938036331, -111.79453520977242    COORDINATES
+//-12.043378791064686, -77.0502811755175   COORDINATES
 //0fd9156e83d87c74cc229489054f52be API KEY
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.82347938036331&lon=-111.79453520977242&appid=0fd9156e83d87c74cc229489054f52be&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-12.043378791064686&lon=-77.0502811755175&appid=0fd9156e83d87c74cc229489054f52be&units=imperial';
 const temp = document.querySelector('#temp');
 const tempImg = document.querySelector('#tempImg');
 
