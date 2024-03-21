4f9316ec5642a42e98c788e339a20eacb9179208
vbisso <valeriabisso19@gmail.com>
2024-03-21 09:57:00 -0600

    spotlight

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,7 +1,10 @@
 const url = 'https://raw.githubusercontent.com/vbisso/wdd230/master/chamber/data/members.json';
-const compcard1 = document.querySelector('#comp1');
-const compcard2 = document.querySelector('#comp2');
-const compcard3 = document.querySelector('#comp3');
+const compimg1 = document.querySelector('#compImg1');
+const comptext1 = document.querySelector('#compText1');
+const compimg2 = document.querySelector('#compImg2');
+const comptext2 = document.querySelector('#compText2');
+const compimg3 = document.querySelector('#compImg3');
+const comptext3 = document.querySelector('#compText3');
 async function getMembers() {
     const response = await fetch(url);
     const data = await response.json();
@@ -24,25 +27,23 @@ function displayMembers(data) {
     let randomIndex2 = Math.floor(Math.random() * list.length);
     let randomIndex3 = Math.floor(Math.random() * list.length);
 
-
-    compcard1.innerHTML = `
-        <img src="${data.companies[randomIndex1].image}" alt="${data.companies[randomIndex1].name}">
-        <p>${data.companies[randomIndex1].name}</p>
+    compimg1.innerHTML = `<img src="${data.companies[randomIndex1].image}" alt="${data.companies[randomIndex1].name}">`;
+    comptext1.innerHTML = `
+    <p>${data.companies[randomIndex1].name}</p>
         <p>${data.companies[randomIndex1].address}</p>
         <p>${data.companies[randomIndex1].phone}</p>
         <p>${data.companies[randomIndex1].website}</p>`
 
-
-    compcard2.innerHTML = `
-        <img src="${data.companies[randomIndex2].image}" alt="${data.companies[randomIndex2].name}">
+    compimg2.innerHTML = `<img src="${data.companies[randomIndex2].image}" alt="${data.companies[randomIndex2].name}">`;
+    comptext2.innerHTML = `
         <p>${data.companies[randomIndex2].name}</p>
-        <p>${data.companies[randomIndex2].address}</p>
-        <p>${data.companies[randomIndex2].phone}</p>
-        <p>${data.companies[randomIndex2].website}</p>`
+            <p>${data.companies[randomIndex2].address}</p>
+            <p>${data.companies[randomIndex2].phone}</p>
+            <p>${data.companies[randomIndex2].website}</p>`
 
-    compcard3.innerHTML = `
-        <img src="${data.companies[randomIndex3].image}" alt="${data.companies[randomIndex3].name}">
-        <p>${data.companies[randomIndex3].name}</p>
+    compimg3.innerHTML = `<img src="${data.companies[randomIndex3].image}" alt="${data.companies[randomIndex3].name}">`;
+    comptext3.innerHTML = `
+    <p>${data.companies[randomIndex3].name}</p>
         <p>${data.companies[randomIndex3].address}</p>
         <p>${data.companies[randomIndex3].phone}</p>
         <p>${data.companies[randomIndex3].website}</p>`
